7cdc512953a5f2d9049a9f69b90622cf4075a6ab
Pranith Pashikanti <pashikantipranith7867@gmail.com>
2023-10-15 08:18:47 +0000

    Progress until 8th October

---
+++ steps/evaluation.py
@@ -0,0 +1,14 @@
+import logging
+from zenml import step
+import pandas as pd
+
+
+@step
+def evaluate_model(df: pd.DataFrame) -> None:
+    """
+    Evaluates the model on the ingested data.
+    Args:
+        df: the ingested data
+
+    """
+    pass
