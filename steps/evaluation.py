4f21f7b3278502825f582585185b5386dd548438
Pranith Pashikanti <pashikantipranith7867@gmail.com>
2023-10-21 12:49:39 +0000

    added experiment tracker

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -6,9 +6,13 @@ from src.evaluation import MSE,RMSE,R2
 from sklearn.base import RegressorMixin
 from typing import Tuple
 from typing_extensions import Annotated
+import mlflow
+
+from zenml.client import Client
+experiment_tracker = Client().active_stack.experiment_tracker
 
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(model: RegressorMixin,
     X_test:pd.DataFrame,
     y_test:pd.DataFrame
@@ -27,12 +31,15 @@ def evaluate_model(model: RegressorMixin,
         prediction = model.predict(X_test)
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("MSE", mse)
 
         r2_class = R2()
         r2 = r2_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("R2", r2)
 
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test,prediction)
+        mlflow.log_metric("RMSE", rmse)
 
         return r2, rmse
 
--- steps/model_train.py
+++ steps/model_train.py
@@ -7,9 +7,12 @@ from src.model_dev import LinearRegressionModel
 from sklearn.base import RegressorMixin
 from .config import ModelNameConfig
 
+from zenml.client import Client
+experiment_tracker = Client().active_stack.experiment_tracker
 
 
-@step
+
+@step(experiment_tracker=experiment_tracker.name)
 def train_df(
     X_train: pd.DataFrame,
     X_test: pd.DataFrame,
@@ -29,6 +32,7 @@ def train_df(
     try:
         model = None
         if config.model_name == "LinearRegression":
+            mlflow.sklearn.autolog()
             model = LinearRegressionModel()
             trained_model = model.train(X_train,y_train)
             return trained_model
