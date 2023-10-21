84c68961262b306941247c6ec875c6427f9f3280
Pranith Pashikanti <pashikantipranith7867@gmail.com>
2023-10-21 13:54:59 +0000

    Deployment in progress

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -1,26 +1,55 @@
 import numpy as np
 import pandas as pd
-from materializer.custom_materializer import cs_materializer 
+# from materializer.custom_materializer import cs_materializer 
 
 from zenml import step,pipeline
 from zenml.config import DockerSettings
 from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
-from zenml.intergrations.constants import mlflow 
-from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import MLflowModelDeployer
-from zenml.integrations.mlflow.steps.mlflow.services import MLflowModelDeployerService
-from zenml.integrations.mlflow.steps.mlflow_steps import mlflow_model_deployer_step
+from zenml.integrations.constants import MLFLOW 
+from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import (
+    MLFlowModelDeployer,
+)
+from zenml.integrations.mlflow.services import MLFlowDeploymentService
+from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
 from zenml.steps import BaseParameters,Output
 
 from steps.clean_data import clean_df
 from steps.evaluation import evaluate_model
 from steps.ingest_data import ingest_df
-from steps.train_model import train_model
+from steps.model_train import train_df
 
-docker_settings = DockerSettings(required_integrations=[mlflow])
-
-
+docker_settings = DockerSettings(required_integrations=[MLFLOW])
 
 
+class DeploymentTriggerConfig(BaseParameters):
+    """ Deployment Trigger Config """
+    min_accuracy: float = 0.92
+
+@step
+def deployment_trigger_step(
+    accuracy: float,
+    config: DeploymentTriggerConfig,
+):  
+    """Implements a simple deployment trigger step. that looks at the input model accuracy and decides if it is good enough to deploy or not."""
+    return accuracy >= config.min_accuracy
 
 
+
+@pipeline(enable_cache=True,settings={"docker": docker_settings})
+def continuous_deployment_pipeline(
+    min_accuracy: float=0.92,
+    workers: int=1,
+    timeout: int=DEFAULT_SERVICE_START_STOP_TIMEOUT,
+):
+    df = ingest_df()
+    X_train,X_test,y_train, y_test = clean_df(df)
+    model = train_df(X_train,X_test,y_train,y_test) 
+    r2_score,rmse = evaluate_model(model,X_test,y_test)
+    deployment_decision = deployment_trigger_step(r2_score)
+    mlflow_model_deployer_step(
+        model=model,
+        deploy_decision=deployment_decision,
+        workers=workers,
+        timeout=timeout,
+    )
 
--- run_deployment.py
+++ run_deployment.py
@@ -1,5 +1,9 @@
-from pipelines.deployment_pipeline import deploy_pipeline,inference_pipeline
+from pipelines.deployment_pipeline import continuous_deployment_pipeline
 import click
+from rich import print
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import MLFlowModelDeployer
+from zenml.integrations.mlflow.services import MLFlowDeploymentService
 
 DEPLOY = "deploy"
 DEPLOY_AND_PREDICT = "deploy_and_predict"
@@ -25,13 +29,63 @@ PREDICT = "predict"
     help="Minimum accuracy required to deploy the model",
 )
 
-def run_pipeline(config: str, min_accuracy: float):
-    if config == DEPLOY:
-        deploy_pipeline(min_accuracy=min_accuracy)
-    elif config == PREDICT:
+def main(config: str, min_accuracy: float):
+    mlflow_model_deployer_component = MLFlowModelDeployer.get_active_model_deployer()
+    deploy = config == DEPLOY or config == DEPLOY_AND_PREDICT
+    predict = config == PREDICT or config == DEPLOY_AND_PREDICT
+
+    if deploy:
+        continuous_deployment_pipeline(
+            min_accuracy=min_accuracy,
+            workers=3,
+            timeout=60,)
+    elif predict:
         inference_pipeline()
-        
+
         # deploy_pipeline(min_accuracy=min_accuracy)
     # elif config == DEPLOY_AND_PREDICT:
         # deploy_pipeline(min_accuracy=min_accuracy)
         # inference_pipeline()
+    print(
+        "You can run:\n "
+        f"[italic green]    mlflow ui --backend-store-uri '{get_tracking_uri()}"
+        "[/italic green]\n ...to inspect your experiment runs within the MLflow"
+        " UI.\nYou can find your runs tracked within the "
+        "`mlflow_example_pipeline` experiment. There you'll also be able to "
+        "compare two or more runs.\n\n"
+    )
+
+    # fetch existing services with same pipeline name, step name and model name
+    existing_services = mlflow_model_deployer_component.find_model_server(
+        pipeline_name="continuous_deployment_pipeline",
+        pipeline_step_name="mlflow_model_deployer_step",
+        model_name="model",
+    )
+
+    if existing_services:
+        service = cast(MLFlowDeploymentService, existing_services[0])
+        if service.is_running:
+            print(
+                f"The MLflow prediction server is running locally as a daemon "
+                f"process service and accepts inference requests at:\n"
+                f"    {service.prediction_url}\n"
+                f"To stop the service, run "
+                f"[italic green]`zenml model-deployer models delete "
+                f"{str(service.uuid)}`[/italic green]."
+            )
+        elif service.is_failed:
+            print(
+                f"The MLflow prediction server is in a failed state:\n"
+                f" Last state: '{service.status.state.value}'\n"
+                f" Last error: '{service.status.last_error}'"
+            )
+    else:
+        print(
+            "No MLflow prediction server is currently running. The deployment "
+            "pipeline must run first to train a model and deploy it. Execute "
+            "the same command with the `--deploy` argument to deploy a model."
+        )
+
+
+if __name__ == "__main__":
+    main()
